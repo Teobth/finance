d04ec8b9f4a3a29764f392706c55f65d5aedd185
Teo <teobth@gmail.com>
2026-04-13 12:39:42 +0200

    V1.6.1 - Visuel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { Historic } from './ts/component_historic';
 import { Analyse } from './ts/component_analyse';
-import { H } from '@angular/cdk/keycodes';
+import { Performance } from './ts/component_performance';
 
 export const routes: Routes = [
     { path: 'analyse', component: Analyse },
     { path: 'analyse/:ticker', component: Analyse },
     { path: 'historique', component: Historic },
+    { path: 'performance', component: Performance },
     { path: '', redirectTo: '/historique', pathMatch: 'full' },
 ];
